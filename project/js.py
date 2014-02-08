04e5b38a0454a51da7839c0bcb16629dc6c027ed
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-02-08 13:12:00 +0100

    rewrote jsbuilder

---
+++ project/js.py
@@ -0,0 +1,25 @@
+import os, lib.file
+
+def build(root, ugly = '-uno', src = 'src', out = 'min'):
+
+	data = os.path.join(root, src)
+	for l in os.listdir(data):
+
+		curr = os.path.join(data, l)
+		if not os.path.isdir(curr) : continue
+
+		path = os.path.join(root, out, '%s.js' % l)
+
+		with open(path, 'w') as f:
+
+			def callback(g):
+				lib.file.read(g, f.write)
+				f.write(os.linesep)
+
+			lib.file.walk(curr, callback)
+
+		if ugly != '-uno' : uglify(path)
+
+
+def uglify(path):
+	os.system('uglifyjs ' + path + ' -o ' + path + ' -m -c')
