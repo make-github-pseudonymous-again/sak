a11f172e0e2a0cc904217d45e94555980699db4a
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-02-08 11:33:39 +0100

    dir walk

--- lib/file.py
+++ lib/file.py
@@ -1,4 +1,4 @@
-import hashlib
+import hashlib, sys, os
 
 def hash(f, h = None, blocksize = 2**15):
 	if h == None : h = hashlib.sha256()
@@ -7,3 +7,12 @@ def hash(f, h = None, blocksize = 2**15):
 		h.update(chunk)
 		chunk = f.read(blocksize)
 	return h
+
+
+def walk(d):
+	for e in sorted(os.listdir(d)):
+		path = os.path.join(d, e)
+
+		if   os.path.isdir(path)  : dcallback(path)
+		elif os.path.isfile(path) : fcallback(path)
+	
