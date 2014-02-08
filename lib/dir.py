6a04ac7c2bf873455e447ead7a948904dd0ea3ad
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-02-08 12:14:57 +0100

    dir walk fix

--- lib/dir.py
+++ lib/dir.py
@@ -1,7 +1,7 @@
 import os, functools
 
 def walk(s, d = None, f = print):
-	if d is None : d = functools.partial(walk, d, f) 
+	if d is None : d = functools.partial(walk, d = d, f = f) 
 	for e in sorted(os.listdir(s)):
 		path = os.path.join(s, e)
 
