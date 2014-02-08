8b32350c62085392e4cfa5e73924ed339203fe86
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-02-08 12:39:33 +0100

    callback based

--- lib/file.py
+++ lib/file.py
@@ -12,11 +12,10 @@ def hash(f, h = None, blocksize = 2**15):
 	read(f, h.update, blocksize)
 	return h
 
-def concat(src, f, blocksize = 2**15):
+def walk(src, f):
 
 	def callback(path):
-		with open(path, 'r') as g : read(g, f.write, blocksize)
-		f.write(os.linesep)
+		with open(path, 'r') as g : f(g)
 
 	lib.dir.walk(src, f = callback)
 	
