37596c9b5432b22d406b9ea85ee7703ffa15c17d
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-02-08 12:12:19 +0100

    dir walk

---
+++ lib/dir.py
@@ -0,0 +1,9 @@
+import os, functools
+
+def walk(s, d = None, f = print):
+	if d is None : d = functools.partial(walk, d, f) 
+	for e in sorted(os.listdir(s)):
+		path = os.path.join(s, e)
+
+		if   os.path.isdir(path)  : d(path)
+		elif os.path.isfile(path) : f(path)
