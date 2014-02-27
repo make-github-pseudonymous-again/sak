0c5898c807cd85791baabf49670a3e938c0ec1f0
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-02-27 23:22:00 +0100

    resize

--- project/img.py
+++ project/img.py
@@ -1,23 +1,31 @@
 import os, subprocess
 
-def svg2eps(img, out = None):
-	helper.do(img, out, '-E', '.eps')
+def x2eps(img, out = None, width = None, height = None):
+	inkscape.convert(img, out, width, height, '-E', '.eps')
 
-def svg2png(img, out = None):
-	helper.do(img, out, '-e', '.png')
+def x2png(img, out = None, width = None, height = None):
+	inkscape.convert(img, out, width, height, '-e', '.png')
 
-def svg2pdf(img, out = None):
-	helper.do(img, out, '-A', '.pdf')
+def x2pdf(img, out = None, width = None, height = None):
+	inkscape.convert(img, out, width, height, '-A', '.pdf')
 
-def eps2svg(img, out = None):
-	helper.do(img, out, '-l', '.svg')
+def x2svg(img, out = None, width = None, height = None):
+	inkscape.convert(img, out, width, height, '-l', '.svg')
+
+def resize(img, out, width, height):
+	subprocess.call(['convert', img, '-resize', '%sx%s' % (width, height), out])
 
 
-class helper:
-	def do(img, out, t, ext):
+class inkscape:
+	def convert(img, out, width, height, t, ext):
 
 		if out is None:
 			base, _ = os.path.splitext(img)
 			out = base + ext
+
+		args = ['inkscape', '-z', '-T', t, out, img]
+
+		if width  is not None : args.extend(['--export-width' , width ])
+		if height is not None : args.extend(['--export-height', height])
 
 		subprocess.call(['inkscape', '-z', '-T', t, out, img])
