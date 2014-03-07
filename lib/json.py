bce1c177159920a9bef213a35091df53f0585d5e
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-03-07 18:15:26 +0100

    json proxy objects

---
+++ lib/json.py
@@ -0,0 +1,19 @@
+import json
+
+
+pretty = lambda *args : json.dump(*args, indent = '\t', separators=(',', ': '))
+
+class proxy:
+
+	def __init__(self, fname, mode = 'r'):
+		self.fname = fname
+		self.data  = None
+		self.mode  = mode
+
+	def __enter__(self):
+		with open(self.fname, 'r') as f : self.data = json.load(f)
+		return self.data
+
+	def __exit__(self, type, value, traceback):
+		if self.mode == 'w':
+			with open(self.fname, 'w') as f : pretty(self.data, f)
