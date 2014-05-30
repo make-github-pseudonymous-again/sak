07854b655d2e4509762f3dc8be3c3dde98000004
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-05-30 19:13:54 +0200

    img + crop

--- sake/img.py
+++ sake/img.py
@@ -12,8 +12,11 @@ def x2pdf(img, out = None, width = None, height = None):
 def x2svg(img, out = None, width = None, height = None):
 	inkscape.convert(img, out, width, height, '-l', '.svg')
 
-def resize(img, out, width, height):
-	subprocess.call(['convert', img, '-resize', '%sx%s' % (width, height), out])
+def resize(img, out, width, height, mode = '^'):
+	subprocess.call(['convert', img, '-resize', '%sx%s%s' % (width, height, mode), out])
+
+def crop(img, out, width, height, gravity = 'Center', left = 0, top = 0):
+	subprocess.call(['convert', img, '-gravity', gravity, '-crop', '%sx%s+%s+%s' % (width, height, left, top), out])
 
 
 class inkscape:
