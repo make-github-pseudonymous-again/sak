fd2fc28a510a1533d2437841fc771c040c9954f1
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-06-14 02:14:22 +0200

    url taking varargs

--- sake/url.py
+++ sake/url.py
@@ -1,8 +1,12 @@
 import lxml.html
 
 
-def href(url):
+def href(url, *args):
 	tree = lxml.html.parse(url)
-	title = tree.find('.//title').text
+	title = tree.find('.//title')
+	if title is not None : text = title.text
+	else : text = url
 	fmt = '<a href="%s">%s</a>'
-	print(fmt % (url, title))
+	print(fmt % (url, text))
+
+	if len(args) : href(*args)
