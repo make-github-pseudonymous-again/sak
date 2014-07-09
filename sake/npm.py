b9767519c1a4c0ad537a711ebc53d0d9f229ba85
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-07-10 01:07:11 +0200

    missing dep + clean output

--- sake/npm.py
+++ sake/npm.py
@@ -4,11 +4,17 @@ import subprocess
 def publish():
 	subprocess.call(['npm', 'publish'])
 
+def build():
+	subprocess.call(['npm', 'run', 'build'])
+
+def test():
+	subprocess.call(['npm', 'test'])	
+
 
 try:
 
 	import semantic_version
-	import os, lib.json, lib.git
+	import os, lib.json, lib.git, lib.list
 
 
 	NPM   = 'package.json'
@@ -19,14 +25,21 @@ try:
 	VERSION_PREFIX = 'v'
 	VERSION_HASH   = 'version'
 
+	SPECIAL  = ['major', 'minor', 'patch']
+
 
 	def release(version, message = None):
 
-		if not semantic_version.validate(version) :
+		special = version in SPECIAL
+		check   = not special
+			
+		if check and not semantic_version.validate(version) :
 			print("version tag '%s' not valid (http://semver.org/)" % version)
 			return
 
-		v = semantic_version.Version(version)
+		if check : v = semantic_version.Version(version)
+
+		olds = []
 
 		for pm in PM:
 			if os.path.isfile(pm):
@@ -36,9 +49,27 @@ try:
 						if not semantic_version.validate(old) :
 							print("old version tag '%s' in '%s' not valid (http://semver.org/)" % (old, pm))
 							return
-						if not v > semantic_version.Version(old):
+						if check and not v > semantic_version.Version(old):
 							print("version tag '%s' should be > than '%s'" % (version, old))
 							return
+
+						olds.append(old)
+
+
+		if special and len(olds) == 0:
+			print("cannot infer version number from '%s' without at least a package configuration file" % version)
+			return
+
+		if not lib.list.one(olds) :
+			print("versions MUST be equal in packages configuration files %s" % list(zip(PM, olds)))
+			return
+
+		if special:
+			v = semantic_version.Version(olds[0])
+			setattr(v, version, getattr(v, version) + 1)
+			version = str(v)
+
+
 
 		for pm in PM:
 			if os.path.isfile(pm):
@@ -49,6 +80,7 @@ try:
 		version = VERSION_PREFIX + version
 		if message is None : message = version
 
+		build()
 		lib.git.add('--all', '.')
 		lib.git.commit('-am', message)
 		lib.git.push()
