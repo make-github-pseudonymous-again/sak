3dd85ddbccfd14b9fd1bac4324df6e623b423c1b
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-08-25 17:29:23 +0200

    byte size human repr util fun

---
+++ lib/bytes.py
@@ -0,0 +1,9 @@
+def human(size):
+	radix = 1024.
+	units = ["B", "KB", "MB", "GB", "TB"]
+	n = len(units)
+	for u in range(n):
+		if size < radix : break
+		size /= radix
+
+	return "%f %s" % (size, units[u])
