0398b889ec8be214d12dea4659b79a2ce1847b2c
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-08-25 11:23:57 +0200

    install for windows (fix #33)

---
+++ winstall.py
@@ -0,0 +1,14 @@
+import os, subprocess
+
+PATH = "PATH"
+PATH_SEP = os.pathsep
+BIN = "bin"
+
+pathlist = os.getenv(PATH).split(PATH_SEP)
+
+froot = os.path.dirname(__file__)
+
+fbin = os.path.join(froot, BIN)
+
+if fbin not in pathlist :
+	subprocess.call(["setx", PATH, fbin])
