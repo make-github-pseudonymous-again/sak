0443f3a8d187128d645b1c53d840a59c03e807c8
aureooms <aurelien.ooms@gmail.com>
2014-08-26 22:21:10 +0200

    Documentation explorer via sake.help.doc. Fix #14.

--- sake/help.py
+++ sake/help.py
@@ -1,19 +1,46 @@
-import inspect, sake, lib
+import inspect, sake, lib.pacman, lib.check, functools
 
-def info(module = None, action = None):
+def ensurefmt(fmt, n):
+	fmt = list(fmt)
+	if not fmt : fmt = [print,]
+	while len(fmt) < n : fmt.append(fmt[-1])
+	return tuple(fmt)
+
+
+def walk(R, module, action, *fmt):
+
+	fmtR, fmtM, fmtA = ensurefmt(fmt, 3)
+
 	if module is None :
-		print(lib.pacman.format(sake, inspect.ismodule))
+		print(fmtR(R))
 
 	else :
-		M = getattr(sake, module, None)
-		lib.check.ModuleDoesNotExistException(M, module, sake)
+		M = getattr(R, module, None)
+		lib.check.ModuleDoesNotExistException(M, module, R)
 
 		if action is None :
-			print(lib.pacman.format(M, inspect.isfunction))
+			print(fmtM(M))
 
 		else :
 
 			A = getattr(M, action, None)
 			lib.check.ActionDoesNotExistException(A, action, M, module)
 
-			print(inspect.formatargspec(*inspect.getargspec(A)))
+			print(fmtA(A))
+
+
+def info(module = None, action = None):
+
+	fmtR = functools.partial(lib.pacman.format, pred = inspect.ismodule)
+	fmtM = functools.partial(lib.pacman.format, pred = inspect.isfunction)
+	fmtA = lambda A : inspect.formatargspec(*inspect.getargspec(A))
+
+	walk(sake, module, action, fmtR, fmtM, fmtA)
+
+
+def doc(module = None, action = None):
+	"""
+		Print the doc of the specified element (default = sake root module)
+	"""
+
+	walk(sake, module, action, inspect.getdoc)
