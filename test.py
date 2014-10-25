a7ca53d4eb16955e4f2a6bb6c7fe355a1d0c5355
aureooms <aurelien.ooms@gmail.com>
2014-10-25 20:49:09 +0200

    doctest file

---
+++ test.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os, doctest
+
+def recurse ( source ) :
+
+	if os.path.isfile( source ) :
+
+		if source[-3:] == ".py" :
+
+			print( source )
+
+			doctest.testfile( source )
+
+	elif os.path.isdir( source ) :
+
+		for child in os.listdir( source ) :
+
+			recurse( os.path.join( source, child ) )
+
+recurse( "lib" )
