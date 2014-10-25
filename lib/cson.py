4186a4bc5de1f6407d863ae316c9b5fa7ecccd45
aureooms <aurelien.ooms@gmail.com>
2014-10-25 18:24:57 +0200

    Coffee Script Object Notation reader and writer

---
+++ lib/cson.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# requires https://github.com/bevry/cson
+
+import os, tempfile, json, lib.sys
+
+
+def load ( fp ) :
+
+	return loads( fp.read() )
+
+
+def loads ( string ) :
+
+	with tempfile.NamedTemporaryFile( "w", delete = False, suffix = ".cson" ) as tmp :
+
+		tmp.write( string )
+
+	out, err, p = lib.sys.call( ["cson2json", tmp.name] )
+
+	os.remove( tmp.name )
+
+	return json.loads( out.decode() )
+
+
+def dump ( obj, fp ) :
+
+	fp.write( dumps( obj ) )
+
+
+def dumps ( obj ) :
+
+	with tempfile.NamedTemporaryFile( "w", delete = False, suffix = ".json" ) as tmp :
+
+		json.dump( obj, tmp )
+
+	out, err, p = lib.sys.call( ["json2cson", tmp.name] )
+
+	os.remove( tmp.name )
+
+	return out.decode()
