40451619af26a717bf2a3a8fcd189b87cd0c8cb8
aureooms <aurelien.ooms@gmail.com>
2014-10-28 12:46:24 +0100

    markdown lib

---
+++ lib/markdown.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+def image ( title, img ) :
+	return "![%s](%s)" % ( title, img )
+
+
+def link ( text, href ) :
+	return "[%s](%s)" % ( text, href )
+
+
+def imagewithlink ( title, img, href ) :
+	text = image( title, img )
+	return link( text, href )
