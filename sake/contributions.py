968c8beab0c842ee86791f8c1f071e5ff040fcbe
aureooms <aurelien.ooms@gmail.com>
2014-11-13 20:48:41 +0100

    order dict keys in contrib.commit

--- sake/contributions.py
+++ sake/contributions.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import lib.git, lib.args, lib.json, lib.time
+import lib.git, lib.args, lib.json, lib.time, collections
 
 def commit ( message, duration, authors = None ) :
 
 	authors = lib.args.listify( authors )
 
-	with lib.json.proxy( "contributions.json", mode = "w", default = [] ) as contribs :
+	hook = collections.OrderedDict
 
-		contribs.append( {
+	with lib.json.proxy( "contributions.json", mode = "w", default = [], object_pairs_hook = hook ) as contribs :
 
-			"authors" : authors,
-			"message" : message,
-			"duration" : duration,
-			"timestamp" : str( lib.time.nanoseconds() )
+		contribs.append( hook( [
 
-		} )
+			( "authors", authors ),
+			( "message", message ),
+			( "duration", duration ),
+			( "timestamp", str( lib.time.nanoseconds() ) )
+
+		] ) )
 
 	lib.git.add( "contributions.json" )
 	lib.git.commit( "-m", message )
