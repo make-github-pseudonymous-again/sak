03f5aafc9e5950aec33f3d8f13f1ed869b8b516d
aureooms <aurelien.ooms@gmail.com>
2014-11-20 21:27:35 +0100

    can now retrieve resume of contributions

--- lib/time.py
+++ lib/time.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import time
+import time , datetime
 
-def nanoseconds () :
-	return int( round( time.time() * 1e9 ) )
+def nanoseconds ( ) :
+	return int( round( time.time( ) * 1e9 ) )
+
+def parsedmy ( dmy ) :
+
+	datefmt = "%d/%m/%Y"
+
+	parsed = datetime.datetime.strptime( dmy , datefmt )
+
+	timetuple = parsed.timetuple( )
+
+	return time.mktime( timetuple ) * 1e9
+
+def pretty ( timestamp ) :
+	return datetime.datetime.utcfromtimestamp( timestamp / 1e9 ).strftime( "%Y-%m-%d %H:%M:%S" )
--- sake/contributions.py
+++ sake/contributions.py
@@ -16,9 +16,64 @@ def commit ( message, duration, authors = None ) :
 			( "authors", authors ),
 			( "message", message ),
 			( "duration", duration ),
-			( "timestamp", str( lib.time.nanoseconds() ) )
+			( "timestamp", str( lib.time.nanoseconds( ) ) )
 
 		] ) )
 
 	lib.git.add( "contributions.json" )
 	lib.git.commit( "-m", message )
+
+
+def manhours ( begin = None , end = None ) :
+
+	if begin is None :
+		begin = 0
+	else :
+		begin = lib.time.parsedmy( begin )
+
+	if end is None :
+		end = lib.time.nanoseconds( )
+	else :
+		end = lib.time.parsedmy( end )
+
+	total = 0
+
+	with lib.json.proxy( "contributions.json", mode = "r", default = [] ) as contribs :
+
+		for contrib in contribs :
+
+			t = int( contrib["timestamp"] )
+
+			if t >= begin and t <= end :
+
+				total += int( contrib["duration"] ) * len( contrib["authors"] )
+
+	print( total )
+
+
+def log ( begin = None , end = None ) :
+
+	if begin is None :
+		begin = 0
+	else :
+		begin = lib.time.parsedmy( begin )
+
+	if end is None :
+		end = lib.time.nanoseconds( )
+	else :
+		end = lib.time.parsedmy( end )
+
+	with lib.json.proxy( "contributions.json", mode = "r", default = [] ) as contribs :
+
+		for contrib in contribs :
+
+			t = int( contrib["timestamp"] )
+
+			if t >= begin and t <= end :
+
+				authors = contrib["authors"]
+				duration = contrib["duration"]
+
+				print( lib.time.pretty( t ) , "~" , " , ".join( authors ) , "(" , duration , ")" )
+				print( " ->" , contrib["message"] )
+				print( "===" )
