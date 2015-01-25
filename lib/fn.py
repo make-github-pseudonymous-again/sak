e4a9ba3ba2ff9f1d408b7637c49d43005ca0d9db
aureooms <aurelien.ooms@gmail.com>
2015-01-25 18:15:19 +0100

    add function throttling decorator

--- lib/fn.py
+++ lib/fn.py
@@ -1,5 +1,5 @@
 
-
+import time
 
 def callable(fn):
 	return hasattr(fn, '__call__')
@@ -8,3 +8,47 @@ def callable(fn):
 def val(o, *args, **kwargs):
 	if callable(o) : return o(*args, **kwargs)
 	else           : return o
+
+
+def throttle ( times , interval ) :
+
+	def wrap ( fn ) :
+
+		count = 0
+		first = time.perf_counter( )
+
+		def wrapper ( *args , **kwargs ) :
+
+			nonlocal count , first
+
+			if count < times :
+
+				count += 1
+
+				fn( *args , **kwargs )
+
+			else :
+
+				now = time.perf_counter( )
+
+				wait = interval - ( now - first )
+
+				if wait <= 0 :
+
+					count = 1
+
+					first = now
+
+					fn( *args , **kwargs )
+
+				else :
+
+					time.sleep( wait )
+
+					wrapper( *args , **kwargs )
+
+		return wrapper
+
+	return wrap
+
+
--- sak/test/do.py
+++ sak/test/do.py
@@ -2,8 +2,19 @@
 	Doc for sak.test.do
 """
 
+import time , itertools , lib.fn , lib.args
+
 def action ( a , b = 10 ) :
 	"""
 		Doc for sak.test.do.action
 	"""
 	print( a , b )
+
+@lib.args.convert( times = int , interval = int )
+def throttled ( times , interval ) :
+
+	@lib.fn.throttle( times , interval )
+	def fn ( i ) :
+		print ( i , time.time() )
+
+	for i in itertools.count( 1 ) : fn( i )
