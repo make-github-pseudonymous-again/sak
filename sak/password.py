b64078eb041fa82ae636b2d6a86bd84360b5979b
aureooms <aurelien.ooms@gmail.com>
2015-06-28 23:10:07 +0200

    use urandom and add bytes generator

--- sak/password.py
+++ sak/password.py
@@ -1,11 +1,20 @@
 
-from random import choice
+from os import urandom
+from random import seed , choice
+import lib.args
 
 def new ( ) :
+
+    seed( urandom( 8 ) )
 
     print( "".join( [ choice(
         "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN`!$%%^&*()_+-=;,./<>?1234567890"
         ) for i in range( 8 ) ] ) )
+
+@lib.args.convert( n = int )
+def bytes ( n ) :
+
+    print( " ".join( map( lambda x : hex( int( x ) )[2:] , urandom( n ) ) ) )
 
 def hexadecimal ( password ) :
 
