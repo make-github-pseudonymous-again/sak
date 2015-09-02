b49e33765d1206e50b46cecdf599c59728f55fa4
aureooms <aurelien.ooms@gmail.com>
2015-09-02 17:10:23 +0000

    fix concat to work with list of files

--- lib/dir.py
+++ lib/dir.py
@@ -1,58 +1,73 @@
 
 import os, functools
 
-def walk(s, d = None, f = print):
-	"""
-		Recursively walks a directory and applies different
-		callback functions depending if the item is a file
-		or a directory.
-	"""
-	if d is None : d = functools.partial(walk, d = d, f = f)
-	for e in sorted(os.listdir(s)):
-		path = os.path.join(s, e)
+def walk(src, d = None, f = print):
+    """
+        Recursively walks a list of files/directories different
+        callback functions depending if the item is a file
+        or a directory.
+    """
+    if isinstance( src , str ) : src = [ src ]
 
-		if   d and os.path.isdir(path)  : d(path)
-		elif f and os.path.isfile(path) : f(path)
+    for s in src :
 
+        if os.path.isdir( s ) : _walk( s , d , f )
+        elif f and os.path.isfile( s ) : f( s )
+
+
+
+def _walk(s, d = None, f = print):
+    """
+        Recursively walks a directory and applies different
+        callback functions depending if the item is a file
+        or a directory.
+    """
+    if d is None : d = functools.partial(walk, d = d, f = f)
+
+    for e in sorted(os.listdir(s)):
+        path = os.path.join(s, e)
+
+        if   d and os.path.isdir(path)  : d(path)
+        elif f and os.path.isfile(path) : f(path)
 
 def makedirs(*dirs):
-	"""
-		Creates all directories given by `dirs` as well as their parents.
-	"""
+    """
+        Creates all directories given by `dirs` as well as their parents.
+    """
 
-	for d in dirs :
-		if not os.path.exists(d) : os.makedirs(d)
+    for d in dirs :
+        if not os.path.exists(d) : os.makedirs(d)
 
 
 def size(*dirs):
-	"""
-		Computes the size of a list of directories.
-	"""
-	# http://stackoverflow.com/a/1392549/1582182
-	total_size = 0
+    """
+            Computes the size of a list of directories.
+    """
+    # http://stackoverflow.com/a/1392549/1582182
+    total_size = 0
 
-	for d in dirs :
-		for dirpath, dirnames, filenames in os.walk(d):
-			for f in filenames:
-				fp = os.path.join(dirpath, f)
-				total_size += os.path.getsize(fp)
+    for d in dirs :
+        for dirpath, dirnames, filenames in os.walk(d):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                total_size += os.path.getsize(fp)
 
-	return total_size
+    return total_size
 
 
 class cd (object) :
 
-	"""
-		http://stackoverflow.com/a/13197763/1582182
-		Context manager for changing the current working directory
-	"""
+    """
+        http://stackoverflow.com/a/13197763/1582182
+        Context manager for changing the current working directory
+    """
 
-	def __init__(self, cwd):
-		self.cwd = cwd
+    def __init__(self, cwd):
+        self.cwd = cwd
 
-	def __enter__(self):
-		self.old = os.getcwd()
-		os.chdir(self.cwd)
+    def __enter__(self):
+        self.old = os.getcwd()
+        os.chdir(self.cwd)
 
-	def __exit__(self, etype, value, traceback):
-		os.chdir(self.old)
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.old)
--- sak/text.py
+++ sak/text.py
@@ -1,21 +1,21 @@
 import os, sys, lib.file, lib.check, fileinput, lib.args
 
-def concat(src, out):
+def concat ( *src , out ) :
 
-	with open(out, 'w') as f:
+    with open( out , 'w' ) as f :
 
-		def callback(g):
-			lib.file.read(g, f.write)
-			f.write(os.linesep)
+        def callback(g):
+            lib.file.read(g, f.write)
+            f.write(os.linesep)
 
-		lib.file.walk(src, callback)
+        lib.file.walk( src , callback )
 
 
 def append(out, txt):
 
-	with open(out, 'a') as f:
-		f.write(txt)
-		f.write(os.linesep)
+    with open(out, 'a') as f:
+        f.write(txt)
+        f.write(os.linesep)
 
 
 TAB = "tab"
@@ -24,65 +24,65 @@ SPACE = "space"
 INDENT_MODES = [TAB, SPACE]
 
 INDENT_DEFAULT_WIDTH = {
-	TAB : 1,
-	SPACE : 4
+    TAB : 1,
+    SPACE : 4
 }
 
 INDENT_CHAR = {
-	TAB : '\t',
-	SPACE : ' '
+    TAB : '\t',
+    SPACE : ' '
 }
 
 def indent(src = sys.stdin, mode = TAB, width = None):
 
-	lib.check.OptionNotInListException("mode", mode, INDENT_MODES)
+    lib.check.OptionNotInListException("mode", mode, INDENT_MODES)
 
-	c = INDENT_CHAR[mode]
+    c = INDENT_CHAR[mode]
 
-	if width is None : width = INDENT_DEFAULT_WIDTH[mode]
+    if width is None : width = INDENT_DEFAULT_WIDTH[mode]
 
-	lineprepend(src, c, width)
+    lineprepend(src, c, width)
 
 
 def lineprepend(src = sys.stdin, string = "", width = 1):
 
-	width = int(width)
-	sequence = string * width
+    width = int(width)
+    sequence = string * width
 
-	if src != sys.stdin :
-		src = open(src, 'r')
+    if src != sys.stdin :
+        src = open(src, 'r')
 
-	for line in src:
-		print(sequence + line, end = '')
+    for line in src:
+        print(sequence + line, end = '')
 
-	if src != sys.stdin :
-		src.close()
+    if src != sys.stdin :
+        src.close()
 
 
 def uppercase ( *args ) :
 
-	for line in fileinput.input( args ) :
+    for line in fileinput.input( args ) :
 
-		print( line.upper() , end = "" )
+        print( line.upper() , end = "" )
 
 
 def lowercase ( *args ) :
 
-	for line in fileinput.input( args ) :
+    for line in fileinput.input( args ) :
 
-		print( line.lower() , end = "" )
+        print( line.lower() , end = "" )
 
 @lib.args.mandatory( n = True )
 @lib.args.convert( n = int )
 def truncate ( *args , n = None ) :
 
-	for line in fileinput.input( args ) :
+    for line in fileinput.input( args ) :
 
-		print( line[:n] , end = "" )
+        print( line[:n] , end = "" )
 
 def split ( *args ) :
 
-	for line in fileinput.input( args ) :
+    for line in fileinput.input( args ) :
 
-		print( *line.split( ) , sep = "\n" , end = "\n" )
+            print( *line.split( ) , sep = "\n" , end = "\n" )
 
