f21a675063820058c01432de8fd84fcb3fd6b017
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-05-28 09:31:03 +0200

    :sparkles: feat(js): Update data files.

--- data/js/doc/scripts/header.js
+++ data/js/doc/scripts/header.js
@@ -17,7 +17,7 @@ domReady(() => {{
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/{repository}';
+	testlink.href = 'https://app.codecov.io/gh/{repository}';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
